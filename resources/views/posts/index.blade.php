0481ee7a7726293dea9d4dcad96f65180d61cabf
yoktak <yokotaku327@gmail.com>
2022-08-28 14:30:49 +0900

    [マイページフロント実装]

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ asset('/CSS/posts/index.css') }}">
         <title>投稿一覧ページ</title>
         
     </head>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,13 +6,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('/CSS/user/index.css') }}">
 
         <title>ユーザーページ</title>
         
     </head>
     <body>
-        <h2>{{Auth::user()->name}}</h2>
         <div class='profile'>
+            <h2>{{Auth::user()->name}}</h2>
             <p>プロフィール</p>
             <h4>{{ Auth::user()->profile }}</h4>
         </div>
@@ -27,28 +28,29 @@
         </div>
         <div class='comics'>
             @foreach(auth()->user()->comics as $comic)
-            <div class='partition'>---------------------------</div>
-            <div class='name'>
-                <p>好きな漫画</p>
-                <h3>{{ $comic->title }}</h3>
-            </div>
-            <div class='read_type'>
-                <p>単行本or雑誌</p>
-                @if($comic->pivot->type===0)
-                    <h3>単行本</h3>
-                @else
-                    <h3>雑誌</h3>
-                @endif
-            </div>
-            <div class='read_episode'>
-                <p>既読巻数/話数</p>
-                <h3>
-                {{ $comic->pivot->episode }}
-                @if($comic->pivot->type===0)巻
-                @else話
-                @endif
-                </h3>
-            </div>
+                <div class='comic'>
+                    <li class='name'>
+                        <p>好きな漫画</p>
+                        <h3>{{ $comic->title }}</h3>
+                    </li>
+                    <li class='read_type'>
+                        <p>単行本or雑誌</p>
+                        @if($comic->pivot->type===0)
+                            <h3>単行本</h3>
+                        @else
+                            <h3>雑誌</h3>
+                        @endif
+                    </li>
+                    <li class='read_episode'>
+                        <p>既読巻数/話数</p>
+                        <h3>
+                        {{ $comic->pivot->episode }}
+                        @if($comic->pivot->type===0)巻
+                        @else話
+                        @endif
+                        </h3>
+                    </li>
+                </div>
             @endforeach
         </div>
     </body>
